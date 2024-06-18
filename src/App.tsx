fb5dd293ba57d0d222fd804f0865b5c0a23ca287
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2024-06-18 19:15:44 +0100

    Refactor App component to include Kanban board and Tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import Kanban from './components/board/Kanban';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Navbar />
+      <div className='max-w-screen-xl mx-auto flex flex-col gap-4'>
+        <h1 className='text-3xl mt-4'>Task management</h1>
+        <Tabs defaultValue='kanban' className='w-[400px]'>
+          <TabsList>
+            <TabsTrigger value='kanban'>Kanban</TabsTrigger>
+            <TabsTrigger value='table'>Table</TabsTrigger>
+          </TabsList>
+          <TabsContent value='kanban'>
+            <Kanban />
+          </TabsContent>
+          <TabsContent value='table'>Change your password here.</TabsContent>
+        </Tabs>
+      </div>
+    </>
+  );
 };
 
 export default App;
