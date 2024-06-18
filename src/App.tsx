1110a2f100f334dd82c2f8494a3f264c60cbb5f4
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2024-06-18 19:28:38 +0100

    Update App component layout and add Kanban tab

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ const App = () => {
   return (
     <>
       <Navbar />
-      <div className='max-w-screen-xl mx-auto flex flex-col gap-4'>
+      <div className='max-w-screen-xl h-[92vh] overflow-y-scroll mx-auto flex flex-col gap-4'>
         <h1 className='text-3xl mt-4'>Task management</h1>
-        <Tabs defaultValue='kanban' className='w-[400px]'>
-          <TabsList>
+        <Tabs defaultValue='kanban' className='w-full '>
+          <TabsList className='w-full flex justify-start'>
             <TabsTrigger value='kanban'>Kanban</TabsTrigger>
             <TabsTrigger value='table'>Table</TabsTrigger>
           </TabsList>
-          <TabsContent value='kanban'>
+          <TabsContent className='h-96' value='kanban'>
             <Kanban />
           </TabsContent>
           <TabsContent value='table'>Change your password here.</TabsContent>
