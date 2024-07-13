10b9c1f4b3fd9159e31743b01fcf11f7620702ac
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2024-07-13 11:57:21 +0100

    Add TaskList component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import Kanban from './components/board/Kanban';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import Navbar from './components/Navbar';
+import TaskList from './components/list/TaskList';
 
 const App = () => {
   return (
-    <>
+    <div className=''>
       <Navbar />
-      <div className='max-w-screen-xl h-[92vh] overflow-y-scroll mx-auto flex flex-col gap-4'>
-        <h1 className='text-3xl mt-4'>Task management</h1>
-        <Tabs defaultValue='kanban' className='w-full '>
+      <div className='h-[92vh] max-w-screen-xl overflow-y-scroll mx-auto flex flex-col gap-8'>
+        <Tabs defaultValue='kanban' className='w-full mt-6'>
           <TabsList className='w-full flex justify-start'>
             <TabsTrigger value='kanban'>Kanban</TabsTrigger>
             <TabsTrigger value='table'>Table</TabsTrigger>
           </TabsList>
-          <TabsContent className='h-96' value='kanban'>
+          <TabsContent className='px-4  h-96' value='kanban'>
             <Kanban />
           </TabsContent>
-          <TabsContent value='table'>Change your password here.</TabsContent>
+          <TabsContent className='px-4' value='table'>
+            <TaskList />
+          </TabsContent>
         </Tabs>
       </div>
-    </>
+    </div>
   );
 };
 
