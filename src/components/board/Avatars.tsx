2b366d50183a349f9b97c53d1640f77277fa24f0
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2024-07-13 11:56:22 +0100

    Refactor Avatars component to use dynamic user data

--- src/components/board/Avatars.tsx
+++ src/components/board/Avatars.tsx
@@ -1,20 +1,21 @@
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 
-export default function Avatars() {
+interface TaskCardProps {
+  users: User[];
+}
+
+const Avatars: React.FC<TaskCardProps> = ({ users }) => {
   return (
     <div className='flex gap-1'>
-      <Avatar>
-        <AvatarImage src='https://github.com/shadcn.png' />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <Avatar>
-        <AvatarImage src='https://github.com/shadcn.png' />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <Avatar>
-        <AvatarImage src='https://github.com/shadcn.png' />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
+      {users &&
+        users.map((user) => (
+          <Avatar key={user.id}>
+            <AvatarImage src={user.img} />
+            <AvatarFallback>{user.name}</AvatarFallback>
+          </Avatar>
+        ))}
     </div>
   );
-}
+};
+
+export default Avatars;
