742c8d9ecf2cbb5c9c76881d0b33d9788f9ec2cf
Lotfi Bouakkaz <bouakkaz144.lotfi@gmail.com>
2024-07-13 11:57:32 +0100

    Refactor UI tabs styling

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -11,7 +11,7 @@ const TabsList = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <TabsPrimitive.List
     ref={ref}
-    className={cn(' border-b-2  text-muted-foreground', className)}
+    className={cn('   text-muted-foreground', className)}
     {...props}
   />
 ));
@@ -24,7 +24,7 @@ const TabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      'inline-flex items-center -mb-0.5 justify-center whitespace-nowrap mr-6 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground data-[state=active]:border-b-2 border-primary',
+      'inline-flex items-center px-4 -mb-0.5 justify-center whitespace-nowrap mr-6 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:text-foreground data-[state=active]:border-b-2 border-primary',
       className
     )}
     {...props}
